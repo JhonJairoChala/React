4af91692e1c08ec7147e39cabac0b29fc3667285
JhonJairoChala <jhonjairochala8@gmail.com>
2022-05-13 15:52:26 -0500

    Agregue el HeaderHome en el proyecto

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,10 +1,11 @@
 import {ButtonUI} from './components/UI/ButtonUI/ButtonUI';
-
+import {Home} from './components/Pages/Home/Home';
 function App() {
   return (
-    <div className="App">
+  <div className="App">
       <h1>Hola</h1>
       <ButtonUI/>
+      <Home/>
     </div>
   );
 }
---
+++ proyecto/src/components/UI/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../../../Images/logo.png';
+
+export const Logo = () => {
+  return (
+    <img src={logo} alt="Logo"/>
+  )
+}
